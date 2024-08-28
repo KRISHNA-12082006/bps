9860ef0249f13553990fbce3093c0dcc9750987f
KRISHNA-12082006 <kkrocks2022@gmail.com>
2024-08-28 16:05:36 +0530

    Implemented model

--- bps/image/models.py
+++ bps/image/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=45)
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Image: {self.title}"
--- bps/image/views.py
+++ bps/image/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Image
 
 # Create your views here.
 def view(request):
-    return render(request, 'index.html')
+    images = Image.objects.all()  # Retrieve all images from the database
+    return render(request, 'index.html', {'images': images})
