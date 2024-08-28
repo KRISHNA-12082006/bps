c789d500161f8f8834dc190c6171f55dd7dbb938
KRISHNA-12082006 <kkrocks2022@gmail.com>
2024-08-28 21:39:53 +0530

    restart

--- bps/image/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Image
-
-# Create your views here.
-def view(request):
-    images = Image.objects.all()  # Retrieve all images from the database
-    return render(request, 'index.html', {'images': images})
